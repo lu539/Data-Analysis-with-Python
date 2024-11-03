9fe20d818a30331aeb4cf3c2d6315df1e4050468
lu539 <lu.lon.lu@yandex.com>
2024-11-03 21:58:11 +0300

    Update submission_template04.py

--- Deep Learning/submission_template04.py
+++ Deep Learning/submission_template04.py
@@ -5,11 +5,55 @@ from torch.nn import functional as F
 
 class ConvNet(nn.Module):
     def __init__(self):
-        ...
+     super().__init__()
+        # 第一个卷积块
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第二个
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第三个
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 全连接层
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64 * 4 * 4, 512)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(512, 10)  # 10类输出
 
     
     def forward(self, x):
-        ...
+  # 第一个卷积块
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+
+        # 第二个
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        # 第三个
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = self.pool3(x)
+
+        # 全连接层
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+ return x   
 
 def create_model():
     return ConvNet()
